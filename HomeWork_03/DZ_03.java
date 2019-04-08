0dbf8103b31c31378f82cf84d4fd0eab5e03b41a
dizotov <dimboz@list.ru>
2019-04-08 13:42:06 +0300

    Завершил подготовку ДЗ

--- HomeWork_03/DZ_03.java
+++ HomeWork_03/DZ_03.java
@@ -30,40 +30,61 @@ import java.util.Scanner;
 public class DZ_03 {
     public static void main(String[] args) {
 
-
-        guessNumber();
-
+        selectGame();
     }
 
-    static void guessNumber (){
+    static void selectGame(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выберите игру:");
+        System.out.println("1. Угадай число");
+        System.out.println("2. Угадай слово");
+        System.out.println("0. Выход");
+
+        switch (sc.nextInt()) {
+            case 1:
+                guessNumberGame();
+                break;
+            case 2:
+                guesVegetableGame();
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Вы ввели неизвестный идентификатор");
+                selectGame();
+        }
+    }
+
+    static void guessNumberGame (){
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
-        boolean playGame = true;
         boolean winGame = false;
         int secretNumb;
 
         System.out.println("* ИГРА: УГАДАЙ ЧИСЛО *");
-        do {
-            secretNumb = random.nextInt(10);
-            System.out.println("Угадайте число от 0 до 10");
-
-            for (int i = 0; i < 3; i++) {
-                if(compareNubers(sc.nextInt(),secretNumb)) {
-                    winGame = true;
-                    break;
-                }
+        secretNumb = random.nextInt(10);
+        System.out.println("Угадайте число от 0 до 10");
+        for (int i = 0; i < 3; i++) {
+            if(compareNubers(sc.nextInt(),secretNumb)) {
+                winGame = true;
+                break;
             }
-            if (!winGame) {
-                System.out.println("Вы не угадали. Загаданное число : " + secretNumb);
-            }
-            playGame = playAgainQuestion();
-        } while (playGame);
+        }
+        if (!winGame) {
+            System.out.println("Вы не угадали. Загаданное число : " + secretNumb);
+        }
+        if (playAgainQuestion()){
+            guessNumberGame();
+        } else {
+            selectGame();
+        };
     }
 
     static boolean compareNubers (int playerNumber, int secretNumber){
+        boolean compare = false;
         if (playerNumber == secretNumber) {
             System.out.println("Вы угадали загаданное число : " + secretNumber);
-            return true;
+            compare = true;
         }
         if (playerNumber > secretNumber) {
             System.out.println("Указанное вами число выше загаданного");
@@ -71,7 +92,61 @@ public class DZ_03 {
         if (playerNumber < secretNumber) {
             System.out.println("Указанное вами число ниже загаданного");
         }
-        return false;
+        return compare;
+    }
+
+    static void guesVegetableGame(){
+        System.out.println("* ИГРА: УГАДАЙ СЛОВО *");
+        boolean gameWin = false;
+        Scanner sc = new Scanner(System.in);
+        String secretVege = randomVege();
+        System.out.println("Введите слово на английском языке");
+        do {
+           if (compareString(sc.nextLine(),secretVege,15)) {
+               gameWin = true;
+
+           }
+        }while (!gameWin);
+        if (playAgainQuestion()){
+            guesVegetableGame();
+        } else {
+            selectGame();
+        };
+    }
+
+    //на вхо передаются параметры слово, слово с которым сравниваем, длинна ширмы из хэштегов
+    static boolean compareString (String userStr, String answerStr, int secretLength ) {
+        boolean compare = false;
+        String display = "";
+
+        for (int i = 0; i < secretLength ; i++) {
+            if (i < userStr.length() && i < answerStr.length()) {
+                if (userStr.equals(answerStr)) {
+                    System.out.println("Вы Угадали загаданное слово: " + userStr);
+                    compare = true;
+                    break;
+                }
+                if (userStr.charAt(i) == answerStr.charAt(i)) {
+                    display += answerStr.charAt(i);
+                    continue;
+                }
+            }
+            display += "#";
+        }
+        System.out.println(display);
+        display = "";
+        return compare;
+    }
+
+    static String randomVege () {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+                "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
+                "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper",
+                "pineapple", "pumpkin", "potato"};
+        Random random = new Random();
+        System.out.println("Компьютер загадал новое слово.");
+        return words[random.nextInt(words.length)];
+
     }
 
     static boolean playAgainQuestion (){
@@ -83,6 +158,7 @@ public class DZ_03 {
                 return true;
             case 0:
                 System.out.println("СПАСИБО ЗА ИГРУ :)");
+                System.out.println("");
                 return false;
             default:
                 System.out.println("Вы ввели неизвестный идентификатор");
